b2210a655dc6cbec3d4840159a2ba1451f76e2ae
hmNakib <hisamnakib29@gmail.com>
2026-02-13 02:27:30 +0600

    Add 1st live class.php file with personalized greeting and user details

--- 1st live class.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$name = "John Doe";
-$age = 25;
-
-
-echo "Hello, $name! Welcome to the live class.\n" ;
-echo "tomar boyosh ki $age ?" ;
---
+++ 1st_live_class.php
@@ -0,0 +1,17 @@
+<?php
+
+$name = "John Doe"; // string data type
+$age = 25; // integer data type or number data type
+$email = "johndoe@example.com";
+
+$name = "Joh Doe";
+$ismale = "True";  // boolean data type
+
+echo "Hello, {$name}! Welcome to the live class.";
+echo "\n";
+echo "tomar boyosh ki {$age} ?";
+echo "\n";
+echo "eita ki tomar $email ?";
+
+
+
