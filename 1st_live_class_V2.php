2751579c19c2907dcfe574eb379b27c86f286041
hmNakib <hisamnakib29@gmail.com>
2026-02-13 02:32:39 +0600

    Add 1st_live_class_V2.php file with user details and greeting messages

---
+++ 1st_live_class_V2.php
@@ -0,0 +1,31 @@
+<?php
+
+$name = "John Doe"; // string data type
+$age = 25; // integer data type or number data type
+$email = "johndoe@example.com";
+
+$name = "Joh Doe";
+$ismale = "True";  // boolean data type
+
+
+echo "Hey {$name}!";
+echo "\n";
+
+echo "Who Old are you? {$age} ";
+
+echo "\n";
+echo "Is this your email? {$email} ";
+
+echo "\n";
+
+
+echo "Hey i am $name . I am $age years old. My email is $email.\n";
+echo "Hey i am $name . I am $age years old. My email is $email.";
+// echo "Hey i am {$name}. I am {$age} years old. My email is {$email} .";
+
+
+/*
+echo "Hey i am $name . I am $age years old. My email is $email.\n";
+echo "Hey i am $name . I am $age years old. My email is $email.";
+// echo "Hey i am {$name}. I am {$age} years old. My email is {$email} .";
+*/
