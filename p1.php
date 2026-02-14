da5506885f4c210e48cc67130663e3c77be66ac7
hmNakib <hisamnakib29@gmail.com>
2026-02-14 17:56:00 +0600

    hlw

--- p1.php
+++ p1.php
@@ -1,6 +1,14 @@
 <?php 
+
 $name = "Hisam";
+
 $x = "12";
 $y = "3";
 $z = "7";
+
+$sum = $x + $y + $z;
+$difference = $x - $y;
+$multiplication = $x * $y;
+
+
 echo "Hello $name, welcome to PHP programming!";
