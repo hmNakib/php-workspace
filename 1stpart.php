d88c39ad3edae7d5ec86f4706d311e2c45818032
hmNakib <hisamnakib29@gmail.com>
2026-02-15 22:11:31 +0600

    hh

--- 1stpart.php
+++ 1stpart.php
@@ -8,4 +8,16 @@ $difference = $x - $y;
 $multiplication = $x * $y;
 
 echo" this is my sum: $sum";
+echo "\n";
+echo "\n";
+echo "this is my difference: $difference";
+echo "\n";
+echo "this is my multiplication: $multiplication";
 
+/*$x = "12";
+$y = "3";
+$z = "7";
+
+$sum = "$x + $y + $z";
+$difference = $x - $y;
+$multiplication = $x * $y;*/
