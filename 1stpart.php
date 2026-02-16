8dc9bea9bc7478f71b2f685171384194e1397bc8
hmNakib <hisamnakib29@gmail.com>
2026-02-16 22:55:09 +0600

    Remove commented-out code from 1stpart.php for clarity

--- 1stpart.php
+++ 1stpart.php
@@ -14,6 +14,15 @@ echo "this is my difference: $difference";
 echo "\n";
 echo "this is my multiplication: $multiplication";
 
+// $x = "12";
+// $y = "3";
+// $z = "7";
+
+// $sum = "$x + $y + $z";
+// $difference = $x - $y;
+// $multiplication = $x * $y;
+
+
 /*$x = "12";
 $y = "3";
 $z = "7";
