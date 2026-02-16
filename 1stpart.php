8f336d68f171e308a235a5e6ae333f0445e2ac41
hmNakib <hisamnakib29@gmail.com>
2026-02-16 22:57:44 +0600

    Remove additional commented-out code in 1stpart.php for improved clarity

--- 1stpart.php
+++ 1stpart.php
@@ -37,3 +37,21 @@ $multiplication = $x * $y;*/
 // echo "this is my difference: $difference";
 // echo "\n";
 // echo "this is my multiplication: $multiplication";
+
+// $name = "John Doe"; // string data type
+// $age = 25; // integer data type or number data type
+// $email = "johndoe@example.com";
+
+// $name = "Joh Doe";
+// $ismale = "True";  // boolean data type
+
+
+// echo "Hey {$name}!";
+// echo "\n";
+
+// echo "Who Old are you? {$age} ";
+
+// echo "\n";
+// echo "Is this your email? {$email} ";
+
+//echo "\n";
