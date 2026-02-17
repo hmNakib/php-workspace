f1ddece5a7aae79936ff68bacce34f846b606636
hmNakib <hisamnakib29@gmail.com>
2026-02-17 23:15:04 +0600

    hlw

--- 1stpart.php
+++ 1stpart.php
@@ -115,29 +115,3 @@ $z = "7";
 
 $sum = "$x + $y + $z";
 $difference = $x - $y;
-$multiplication = $x * $y;*/
-
-// echo" this is my sum: $sum";
-// echo "\n";
-// echo "\n";
-// echo "this is my difference: $difference";
-// echo "\n";
-// echo "this is my multiplication: $multiplication";
-
-// $name = "John Doe"; // string data type
-// $age = 25; // integer data type or number data type
-// $email = "johndoe@example.com";
-
-// $name = "Joh Doe";
-// $ismale = "True";  // boolean data type
-
-
-// echo "Hey {$name}!";
-// echo "\n";
-
-// echo "Who Old are you? {$age} ";
-
-// echo "\n";
-// echo "Is this your email? {$email} ";
-
-//echo "\n";
